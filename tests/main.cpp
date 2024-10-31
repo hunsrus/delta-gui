6985147b642e0477c9da74adce7feaeb6237fa8c
hunsrus <gabie00018@gmail.com>
2024-10-31 14:58:32 -0300

    test only  motor1

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,19 +4,23 @@
 
 #define DIR1 22
 #define STEP1 23
+/*
 #define DIR2 24
 #define STEP2 25
 #define DIR3 26
 #define STEP3 27
 #define DIR4 33
 #define STEP4 32
-#define MS1 28
-#define MS2 29
-#define MS3 30
+*/
+#define MS1 24
+#define MS2 25
+#define MS3 26
+/*
 #define MS1B 35
 #define MS2B 36
 #define MS3B 37
 #define BOMBA 39
+*/
 
 int main(int argc, char** argv)
 {
@@ -31,65 +35,71 @@ int main(int argc, char** argv)
     
     gpioSetMode(DIR1,PI_OUTPUT);
     gpioSetMode(STEP1,PI_OUTPUT);
+    /*
     gpioSetMode(DIR2,PI_OUTPUT);
     gpioSetMode(STEP2,PI_OUTPUT);
     gpioSetMode(DIR3,PI_OUTPUT);
     gpioSetMode(STEP3,PI_OUTPUT);
     gpioSetMode(DIR4,PI_OUTPUT);
     gpioSetMode(STEP4,PI_OUTPUT);
+    */
     gpioSetMode(MS1,PI_OUTPUT);
     gpioSetMode(MS2,PI_OUTPUT);
     gpioSetMode(MS3,PI_OUTPUT);
+    /*
     gpioSetMode(MS1B,PI_OUTPUT);
     gpioSetMode(MS2B,PI_OUTPUT);
     gpioSetMode(MS3B,PI_OUTPUT);
     gpioSetMode(BOMBA,PI_OUTPUT);
+    */
 
     gpioWrite(MS1,1);
     gpioWrite(MS2,1);
     gpioWrite(MS3,1);
+    /*
     gpioWrite(MS1B,0);
     gpioWrite(MS2B,0);
     gpioWrite(MS3B,0);
     gpioWrite(BOMBA,0);
+    */
 
     while(true)
     {
-        gpioWrite(BOMBA, 0);
+        //gpioWrite(BOMBA, 0);
         gpioWrite(DIR1, 0);
-        gpioWrite(DIR2, 0);
-        gpioWrite(DIR3, 0);
-        gpioWrite(DIR4, 0);
+        //gpioWrite(DIR2, 0);
+        //gpioWrite(DIR3, 0);
+        //gpioWrite(DIR4, 0);
         for(i=0; i<1000; i++){
             gpioWrite(STEP1, 1);
-            gpioWrite(STEP2, 1);
-            gpioWrite(STEP3, 1);
-            gpioWrite(STEP4, 1);
+            //gpioWrite(STEP2, 1);
+            //gpioWrite(STEP3, 1);
+            //gpioWrite(STEP4, 1);
             usleep(1500);
             gpioWrite(STEP1, 0);
-            gpioWrite(STEP2, 0);
-            gpioWrite(STEP3, 0);
-            gpioWrite(STEP4, 0);
+            //gpioWrite(STEP2, 0);
+            //gpioWrite(STEP3, 0);
+            //gpioWrite(STEP4, 0);
             usleep(1500);
         }
         i = 0;
-        gpioWrite(BOMBA, 1);
+        //gpioWrite(BOMBA, 1);
             usleep(2000);
         //  gpioWrite(GATE, 1);
         gpioWrite(DIR1, 1);
-        gpioWrite(DIR2, 1);
-        gpioWrite(DIR3, 1);
-        gpioWrite(DIR4, 1);
+        //gpioWrite(DIR2, 1);
+        //gpioWrite(DIR3, 1);
+        //gpioWrite(DIR4, 1);
         for(i=0; i<1000; i++){
             gpioWrite(STEP1, 1);
-            gpioWrite(STEP2, 1);
-            gpioWrite(STEP3, 1);
-            gpioWrite(STEP4, 1);
+            //gpioWrite(STEP2, 1);
+            //gpioWrite(STEP3, 1);
+            //gpioWrite(STEP4, 1);
             usleep(1500);
             gpioWrite(STEP1, 0);
-            gpioWrite(STEP2, 0);
-            gpioWrite(STEP3, 0);
-            gpioWrite(STEP4, 0);
+            //gpioWrite(STEP2, 0);
+            //gpioWrite(STEP3, 0);
+            //gpioWrite(STEP4, 0);
             usleep(1500); 
         }
     }
